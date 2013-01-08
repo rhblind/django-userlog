b2fd1df8a9899d6c235c51f43469143347d5a050
Rolf Håvard Blindheim <rhblind@gmail.com>
2013-01-08 22:11:30 +0100

    added message constructor

--- userlog/views.py
+++ userlog/views.py
@@ -1,9 +1,54 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from userlog.models import post_writelog
+from django.utils.text import get_text_list
 from django.utils.encoding import force_unicode
+from django.utils.translation import ugettext as _
 from django.contrib.contenttypes.models import ContentType
 from userlog.models import UserLogEntry, ADDITION, CHANGE, DELETION
+
+
+def construct_change_message(request, form, formsets=None):
+    """
+    Constructs a change message from a changed form
+    and optionally a list of formsets.
+    """
+    
+    change_message = []
+    labels = None
+    use_labels = getattr(settings, "USERLOG_LABEL_NAMES", False)
+    
+    if form.changed_data:
+        if use_labels is True:
+            labels = [form.fields[field].label for field in form.changed_data]
+        change_message.append(_("Changed %s." % get_text_list(labels or form.changed_data, _("and"))))
+    
+    if formsets:
+        for formset in formsets:
+            # New objects
+            for obj in formset.new_objects:
+                change_message.append(_('Added %(name)s "%(object)s".')
+                                      % {"name": force_unicode(obj._meta.verbose_name),
+                                         "object": force_unicode(obj)})
+            # Changed objects
+            for obj, changed_fields in formset.changed_objects:
+                labels = None
+                # TODO: figure out a reliable way of solving this
+#                if use_labels is True:
+#                    labels = [f.verbose_name for f in obj._meta.fields if f.name in changed_fields]
+                change_message.append(_('Changed %(list)s for %(name)s "%(object)s".')
+                                      % {"list": get_text_list(labels or changed_fields, _("and")),
+                                         "name": force_unicode(obj._meta.verbose_name),
+                                         "object": force_unicode(obj)})
+            # Deleted objects
+            for obj in formset.deleted_objects:
+                change_message.append(_('Deleted %(name) "%(object)".')
+                                      % {"name": force_unicode(obj._meta.verbose_name),
+                                         "object": force_unicode(obj)})
+
+    change_message = " ".join(change_message)
+    return change_message or _("No fields changed.")
 
 
 class UserLogCreateMixin(object):
@@ -17,7 +62,9 @@ class UserLogCreateMixin(object):
         # Only write log for models which are
         # configured to be logged.
         if getattr(self.object, "log_useractions", False):
-            self.write_log(self.request, self.object, "message")
+            message = construct_change_message(self.request, form)
+            self.write_log(self.request, self.object, message)
+
         return super(UserLogUpdateMixin, self).form_valid(form)
     
     def write_log(self, request, obj, message):
@@ -44,6 +91,7 @@ class UserLogUpdateMixin(UserLogCreateMixin):
             content_type_id=ContentType.objects.get_for_model(obj).pk,
             object_id=obj.pk, object_repr=force_unicode(obj), action_flag=CHANGE,
             change_message=message)
+        post_writelog.send(sender=UserLogEntry, instance=obj, using=obj._state.db)
 
         
 class UserLogDeleteMixin(object):
@@ -53,8 +101,12 @@ class UserLogDeleteMixin(object):
     
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
+        
+        # Only write log for models which are
+        # configured to be logged.
         if getattr(self.object, "log_useractions", False):
             self.write_log(self.request, self.object, "message")
+
         return super(UserLogDeleteMixin, self).delete(request, *args, **kwargs)
     
     def write_log(self, request, obj, message):
@@ -66,6 +118,7 @@ class UserLogDeleteMixin(object):
             content_type_id=ContentType.objects.get_for_model(obj).pk,
             object_id=obj.pk, object_repr=force_unicode(obj), action_flag=DELETION,
             change_message=message)
+        post_writelog.send(sender=UserLogEntry, instance=obj, using=obj._state.db)
 
 
 class FullHistoryMixin(object):
